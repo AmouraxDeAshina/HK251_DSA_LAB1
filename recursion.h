936f76b6457e70189bc002f1a2aba2a42632b210
AmouraxDeAshina <amourax.de.ashina.2411443@gmail.com>
2025-09-23 20:28:54 +0700

    move question from recursion.h to README

---
+++ recursion.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <iostream>
+using namespace std;
+void printArray(int n);
+void printPattern(int n);
+int findMax(int* arr, int length);
+bool isPalindrome(string str);
+int findGCD(int a, int b);
